45e9305182599c61c94719562e1edd625dc95021
jackthomas233 <145718012+jackthomas233@users.noreply.github.com>
2024-04-25 16:01:42 -0500

    Add files via upload

---
+++ neuralnetwork_functions.py
@@ -0,0 +1,37 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import json
+with open('config.json', 'r') as f:
+    config = json.load(f)
+v_r = config['v_r']
+v_thr = config['v_thr']
+v_spike = config['v_spike']
+v_rev = config['v_rev']
+tao_m = config['tao_m']
+tao_syn = config['tao_syn']
+c_m = config['c_m']
+g_bar = config['g_bar']
+t_r = config['t_r']
+w = config['w']
+dt = config['dt']
+# initialize other variables
+t = 0
+def t0():
+    t_spike_train = np.arrange(0, 100, (100 - 0) /)
+    t -
+
+duration = 100
+
+def S(t, t_s):
+    return 0 if t - t_s - t_r <= 0 else 1
+def LIF_neuron_model(v_m, t, I_syn):
+    return ((-(v_m-v_r)/tao_m) + (I_syn/c_m))
+
+while t < duration:
+    t = t + dt #Increase step size
+    I_syn = 1 * ((v_rev - v_m)*)
+
+
+
+
+
