ab8a762b3aa7fb338d8d958335a0c325aa314c4e
AANguyen220 <157168843+AANguyen220@users.noreply.github.com>
2024-04-27 14:03:32 -0500

    Correctly takes in command inputs

--- neuro_sim.py
+++ neuro_sim.py
@@ -12,32 +12,27 @@ t = 0
 
 if __name__ == "__main__":
 
-    # Add arguments
-
+    # Add argument
     parser = argparse.ArgumentParser(description='Simulate single LIF neuron alpha synapse using numerical methods.')
 
-    # Define group for '-m' which will require '--spike_rate' and '--current' if either mode is selected
-    group = parser.add_mutually_exclusive_group(required=True)
+    # '-m' mode of the experiment (current or spike)
+    parser.add_argument("m", choices=["current", "spike"], help="Simulation mode: 'current' or 'spike'")
 
-    # '-m' argument for the mode of simulation
-    group.add_argument('-m', type=str, help='Simulation mode (spike or current)', choices=['spike', 'current'],)
+    # '-s' runtime of simulation (milliseconds)
+    parser.add_argument("s", type=float, help="Simulation time in milliseconds.")
 
-    # '-s' argument for how long to run the simulation in milliseconds
-    parser.add_argument('-s', type=float, help='Simulation run-time (milliseconds)', required=True,)
-
-    # '--spike_rate' argument required if 'spike' is chosen
-    parser.add_argument('--spike_rate', type=int, help='Input spike rate in Hz (Only required for "spike" mode)',)
-
-    # '--current' argument required if 'current' is chosen
-    parser.add_argument('--current', type=float, help='Specify input current in nA (Only required for "current" mode)',)
-
+    # '--spike_rate' and '--current'
+    parser.add_argument("--spike_rate", type=int, help="Spike rate in Hz (required for 'spike' mode).")
+    parser.add_argument("--current", type=float, help="Input current in nanoamps (required for 'current' mode).")
     args = parser.parse_args()
 
     # Error check
     if args.m == 'spike' and not args.spike_rate:
         parser.error('--spike_rate is required when mode is "spike"')
-    if args.m == 'current' and not args.current:
+    elif args.m == 'current' and not args.current:
         parser.error('--current is required when mode is "current"')
+
+
 
     duration = 100
 
