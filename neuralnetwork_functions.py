d6714405b23adfd78b24cdad8244ebc1f4138727
jackthomas233 <jtho487@lsu.edu>
2024-04-28 19:27:14 -0500

    Update neuralnetwork_functions.py
    update neuralnetwork file

--- neuralnetwork_functions.py
+++ neuralnetwork_functions.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import argparse
 
 import json
+
 with open('config.json', 'r') as f:
     config = json.load(f)
 v_r = config['v_r']
@@ -17,53 +18,46 @@ t_r = config['t_r']
 w = config['w']
 dt = config['dt']
 
-def t0(hertz): #Equation 1
-    t_spike_train = np.arrange(0, 100, (100 - 0) / hertz )
-    for i in t_spike_train:
-        t0 = t - t_spike_train[i]
-        if(t0>0):
-            return t0
-
-duration = 100
-
+def synaptic_current_term(v_m, input_current):
+    return -(v_m - v_r) / tau_m + input_current / c_m
 def S(t, t_s):
-    return 0 if t - t_s - t_r <= 0 else 1 #Equation 2 piecewise
+    return 0 if t - t_s - t_r <= 0 else 1  # Equation 2 piecewise
+def isyn(v_m, t, t0, local_w):
+    synaptic_current = local_w * g_bar * (v_rev - v_m) * ((t - t0) / tao_syn) * np.exp(-(t - t0) / tao_syn)
+    # isyn = g * ((v_r-v_m)*((t-t0)/tao_syn))**(-(t-t0)/tao_syn) #This should be right for equation 3, but there may be
+    return synaptic_current  # a difference. Not sure how the exponent is supposed to work.
+def dvm_dt(t, v_m, v_r, tau_m, input_current, c_m, t_s, t_r):
+    return synaptic_current_term(v_m, input_current) * S(t, t_s)
+def euler_vm(v_m, input_current, c_m, t_r, dt, t, v_r, tau_m):
+    dv_dt = dvm_dt(t, v_m, v_r, tau_m, input_current, c_m, t_s, t_r)
+    v_m_next = v_m + dvm_dt * dt
+    return v_m_next
 
-def isyn(v_m, t, t0, w):
-    isyn = g_bar * ((v_rev - v_m) * ((t - t0) /tao_syn)) * np.exp(-(t - t0)/tao_syn)
-    #isyn = g * ((v_r-v_m)*((t-t0)/tao_syn))**(-(t-t0)/tao_syn) #This should be right for equation 3, but there may be
-    return isyn                                      #a difference. Not sure how the exponent is supposed to work.
 def LIF_model(mode, t, spike_rate=None, current=None):
-    steps = int(t/dt)
-    vm = np.zeros(steps)
-    time = np.linspace(0,t,steps)
-    for i in range (1,steps):
-        if(vm[i-1]>=v_thr):
-            vm[i]=v_r
+    steps = int(t / dt)
+    v_m = np.zeros(steps)
+    time = np.linspace(0, t, steps)
+    local_w = w
+    for i in range(1, steps):
+        if v_m[i - 1] >= v_thr:
+            v_m[i] = v_r
         else:
             if mode == "current":
                 input_current = current
             elif mode == "spike":
                 input_current = isyn(
-                    vm[i - 1], time[i], time[0], 1
+                    v_m[i - 1], time[i], time[0], 1
                 )
                 input_current *= spike_rate / 1000  # Convert Hz to kHz
+                v_m[i] = euler_vm(v_m[i - 1], input_current, c_m, t_r, dt, time[i], v_r, tao_m)
             else:
                 raise ValueError("Invalid mode. Mode must be 'current' or 'spike'.")
 
-            vm[i] = (
-                vm[i - 1]
-                + (input_current / c_m) * dt
-                - (vm[i - 1] - v_r)
-                * dt
-                / t_r)
-    plt.plot(time, vm)
+    plt.plot(time, v_m)
     plt.xlabel("Time (s)")
     plt.ylabel("Membrane Voltage (V)")
     plt.title("Membrane Voltage vs Time")
     plt.show()
-
-
 
 def main():
     # Add argument
@@ -86,17 +80,17 @@ def main():
     elif args.m == 'current' and not args.current:
         parser.error('--current is required when mode is "current"')
 
-    LIF_model(args.m, args.s, args.spike_rate)
+    LIF_model(args.m, args.s, args.spike_rate, args.current)
 
-
-
-    #GET VM USING EULER'S METHOD
+    # GET VM USING EULER'S METHOD
+    '''
     while t < duration:
         t = t + dt  # Increase step size
         v_m=0
         t0=0
         I_syn = 1 * ((v_rev - v_m) * ((t - t0(args.spike_rate())) / tao_syn) * (np.exp(-(t - t0()) / tao_syn)))
-        v_m = (v_m+dt) * LIF_neuron_model(v_m, t, I_syn)
+        v_m = (v_m+dt) * LIF_model(v_m, t, I_syn)
+    '''
 
 
 if __name__ == "__main__":
