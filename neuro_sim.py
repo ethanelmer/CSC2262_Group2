e167e2adb86ce62dca24274c20f09362070f1e9e
Ethan Elmer <ethankelmer@gmail.com>
2024-04-30 13:35:47 -0500

    Wrapping up project

--- neuro_sim.py
+++ neuro_sim.py
@@ -20,34 +20,33 @@ dt = config['dt']
 
 def synaptic_current_term(v_m, input_current):
     return -(v_m - v_r) / tao_m + input_current / c_m
+
+#Equation 2 Piecewise
 def S(t, t_s):
-    return 0 if t - t_s - t_r <= 0 else 1  # Equation 2 piecewise
+    return 0 if t - t_s - t_r <= 0 else 1
+
 # Function for alpha synapse
 def isyn(v_m, t, t0):
-    # Ensure t0 is not negative infinity and t - t0 is positive
     if t0 == -np.inf or t - t0 < 0:
         return 0
-    # Calculate the synaptic current using the alpha function
     time_since_spike = (t - t0)
     if time_since_spike < 0:
-        return 0  # This ensures no synaptic input is calculated before the first spike
+        return 0
     synaptic_current = w * g_bar * (v_rev - v_m) * (time_since_spike / tao_syn) * np.exp(-(time_since_spike) / tao_syn)
     return synaptic_current
 
-#Function to define ODE (multiplies S() and synaptic_current_term() functions
+#Function to define ODE
 def dvm_dt(t, v_m, input_current, t_s):
     return synaptic_current_term(v_m, input_current) * S(t, t_s)
 
 
 def LIF_model(mode, t, spike_rate, current=None):
+    #Initialize variables
     steps = int(t / dt)
-    v_m = np.full(steps, v_r)  # Initialize with the resting potential
+    v_m = np.full(steps, v_r)
     time = np.linspace(0, t, steps)
-
-
-    # Variables for tracking spikes and input currents
-    last_spike_time = -np.inf  # Time of the last neuron's spike
-    last_input_spike_time = 0  # Time of the last input spike from presynaptic neuron
+    last_spike_time = -np.inf
+    last_input_spike_time = 0
 
     for i in range(1, steps):
         # Get the last input spike time
@@ -101,7 +100,6 @@ def main():
     # Convert simulation time from milliseconds to seconds
     sim_time_seconds = args.sim_time / 1000
 
-    # Error checks
     if args.mode == 'spike' and args.spike_rate is None:
         parser.error('--spike_rate is required when mode is "spike"')
     elif args.mode == 'current' and args.current is None:
@@ -109,7 +107,6 @@ def main():
 
     # Run the model
     LIF_model(args.mode, sim_time_seconds, args.spike_rate, args.current)
-
 
 if __name__ == "__main__":
     main()
